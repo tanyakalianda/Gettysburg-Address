516e27132f51e0cd2415174a4c7ad8dccd3fe209
tanyakalianda <42977166+tanyakalianda@users.noreply.github.com>
2018-11-07 22:15:07 -0500

    Add files via upload

--- src/GettysburgAddress.java
+++ src/GettysburgAddress.java
@@ -8,20 +8,53 @@ public class GettysburgAddress
 
 	public static void main(String[] args) throws FileNotFoundException 
 	{
-		Scanner file = new Scanner (new File("GettysburgAdress.txt"));
+		Scanner file = new Scanner (new File("GettysburgAddress.txt"));
 		
 		ArrayList<String> speech = new ArrayList<String>();
-		String word = "";
 		
 		while (file.hasNext())
 		{
-			word = file.next();
-			speech.add(word);
+			speech.add(file.next());
+			file.nextLine();
+		}
+		
+//		int i = 0;
+//		String longestWord = "";
+//		while (i < speech.size())
+//		{
+//			String word1 = speech.get(i);
+//			int compare = word1.compareTo(longestWord);
+//			if (compare > 0)
+//			{
+//				longestWord = word1;
+//			}
+//			i++;
+//		}
+//		System.out.println("Longest word: " + longestWord);
+		
+		String longestWord = speech.get(0);
+		for (int i = 1; i < speech.size(); i ++)
+		{
+			int compare = speech.get(i).compareTo(longestWord);
+			if (compare > 0)
+			{
+				longestWord = speech.get(i);
+			}
+		}
+		System.out.println("Longest word: " + longestWord);
+		
+		
+		double total = 0;
+		for (String word: speech)
+		{
+			total = total + word.length();
 		}
 		
 		
+		double average = total / speech.size();
 		
-
+		System.out.println("Average word length: " + average);
+		
 	}
 
 }
